e253f52f45da3b28f04fddb5279bc2422203d223
BaZz Ychnightder <pierreychnightder561@gmail.com>
2025-04-17 21:44:43 +0200

    debut navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <Header />
-
   <h1>Hello Vue 🚀</h1>
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
 </script>
+
+<style>
+
+</style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,20 +1,58 @@
 <template>
   <header class="header">
-    <div class="logo">🚀 SpaceTourism</div>
+    <div class="logo">
+      <img src="../assets/images/shared/logo.svg" alt="star">
+    </div>
+      <div class="separator"></div>
     <nav class="nav">
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Destinations</a></li>
-        <li><a href="#">Crew</a></li>
-        <li><a href="#">Technology</a></li>
+        <li v-for="(link , i) in links" :key="link.name">
+          <a @click="activeLink = link.name" href="#">{{ formatIndex(i) }} {{ link.name.toUpperCase() }}</a>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
 <script setup>
+import { ref } from 'vue';
 
+const links = [
+  { name: 'Home' },
+  { name: 'Destination' },
+  { name: 'Crew' },
+  { name: 'Technology' }
+];
+
+const activeLink = ref('Home');
+
+function formatIndex(i) {
+  return i.toString().padStart(2, '0');
+}
 </script>
 
 <style  lang="scss"  scoped>
+
+  .header{
+    width: 100%;
+    height: 136px;
+    display: flex;
+    align-items: flex-end;
+
+    .separator{
+      width: 473px;
+      height: 1px;
+      background-color: white;
+    }
+    .nav{
+      width: 830px;
+      height: 96px;
+      display: flex;
+      ul{
+        display: flex;
+      }
+    }
+
+  }
+
 
 </style>
